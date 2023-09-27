cc4f2925612b692e134b310cc1b237ff88e7a896
ARitz Cracker <aritz@aritzcracker.ca>
2023-09-27 21:23:32 +0000

    fix "main" export

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = Buffer;
+module.exports = {
+	Buffer,
+	// eslint-disable-next-line object-shorthand
+	SlowBuffer: function(len){
+		return Buffer.allocUnsafeSlow(len);
+	},
+	INSPECT_MAX_BYTES: 0,
+	kMaxLength: 0x7fffffff
+};
